9a05db82cb98b46b8b9c0401b349faa07ffaa9de
rakib544 <md.rakib10122003@gmail.com>
2026-08-04 23:31:28 +0600

    fix: fixed the mod issue of dice-combinations solution

--- Dynamic Programming/dice-combinations/solution.cpp
+++ Dynamic Programming/dice-combinations/solution.cpp
@@ -16,7 +16,7 @@ int main() {
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= 6; j++) {
             if(i - j >= 0) {
-                dp[i] = (dp[i] + dp[i-j] % mod);
+                dp[i] = (dp[i] + dp[i-j]) % mod;
             }
         }
     }
